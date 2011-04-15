2487b5ed250a288a5e6ebfc2566e4b694e43b6c4
Richard Ma <richard.ma.19850509@gmail.com>
2011-04-15 22:49:19 +0800

    Add matching directory.

---
+++ string/matching/pattern/pattern.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define  MAX_SIZE 256
+
+/* 朴素的匹配算法 */
+int basic_pattern(char *source, char *pattern) {
+    int i, j, ans, flg, ls, lp;
+
+    ls = strlen(source); lp = strlen(pattern);
+    ans = -1;
+
+    for (i = 0; i < ls; i++) {
+        flg = 1;
+        for (j = 0; j < lp; j++) {
+            if (source[i] == pattern[j]) {
+                i++;
+                continue;
+            } else {
+                flg = 0;
+                break;
+            }
+        }
+        if (flg == 1) {
+            ans = i - lp;
+            break;
+        }
+    }
+
+    return ans;
+}
+
+int main(int argc, const char *argv[]) {
+    
+    char source[MAX_SIZE], pattern[MAX_SIZE];
+    scanf("%s", source);
+    scanf("%s", pattern);
+
+    /* basic pattern */
+    int ans = basic_pattern(source, pattern);
+    printf("%d\n", ans);
+
+    return 0;
+}
