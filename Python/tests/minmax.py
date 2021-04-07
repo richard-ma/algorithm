6fe43009e503b89c91f804811300d459277eca9a
Richard Ma <richard.ma.19850509@gmail.com>
2021-04-07 22:32:47 +0800

    add empty_array and one_item_array tests

--- Python/tests/minmax.py
+++ Python/tests/minmax.py
@@ -31,6 +31,20 @@ class MinMaxTestCase(MyTestCase):
         self.assertEqual(min_by_min, min_by_min_max)
         self.assertEqual(max_by_max, max_by_min_max)
 
+    def test_min_max_with_empty_array(self):
+        empty_array = list()
+        min_by_min_max, max_by_min_max = min_max(empty_array)
+
+        self.assertIsNone(min_by_min_max)
+        self.assertIsNone(max_by_min_max)
+
+    def test_min_max_with_one_item_array(self):
+        one_item_array = [random.randint(self.l_bound, self.u_bound)]
+        min_by_min_max, max_by_min_max = min_max(one_item_array)
+
+        self.assertEqual(one_item_array[0], min_by_min_max)
+        self.assertEqual(one_item_array[0], max_by_min_max)
+
 
 if __name__ == '__main__':
     unittest.main()
