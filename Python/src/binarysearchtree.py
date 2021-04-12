f1e51ec63ace8bc43fd4a7e0fab259f4dabed7b3
Richard Ma <richard.ma.19850509@gmail.com>
2021-04-12 09:29:44 +0800

    add is_none function

--- Python/src/binarysearchtree.py
+++ Python/src/binarysearchtree.py
@@ -65,6 +65,9 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
+    def is_none(self, x):
+        return x is None
+
     def inorder_walk(self, root):
         if root is not None:
             self.inorder_walk(root.left)
