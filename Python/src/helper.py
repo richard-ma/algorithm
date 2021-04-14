cb479be3e48267cf3d19f7a64fd241055694a2d5
Richard Ma <richard.ma.19850509@gmail.com>
2021-04-14 19:38:50 +0800

    impeletement breadth first search

--- Python/src/helper.py
+++ Python/src/helper.py
@@ -2,3 +2,20 @@ from sys import maxsize
 
 INT_MAX = maxsize
 INT_MIN = -maxsize-1
+
+
+def breadth_first_search(root, none=None):
+    queue = [root]
+
+    ptr = 0
+    while ptr < len(queue):
+        node = queue[ptr]
+
+        if node.left != none:
+            queue.append(node.left)
+        if node.right != none:
+            queue.append(node.right)
+
+        ptr += 1
+
+    return queue
