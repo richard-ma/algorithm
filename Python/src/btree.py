5aed5aef2b3ecc1a591659a668320077d0c7c80f
Richard Ma <richard.ma.19850509@gmail.com>
2021-04-21 10:05:13 +0800

    add B tree

---
+++ Python/src/btree.py
@@ -0,0 +1,107 @@
+from Python.src.helper import *
+
+
+class BTree:
+    class Node:
+        def __init__(self, t):
+            self.keys = [INT_MIN] * (2 * t - 1)
+            self.childs = [None] * (2 * t)
+            self.leaf = True
+            self.n = 0
+
+    def __init__(self, t=2, none=None):
+        self.t = t
+        self.none = none
+        self.root = self.none
+
+        self._create()
+
+    def _print(self, root):
+        print('[', root.n, ']', sep='', end=' ')
+        for i in range(0, root.n):
+            print(root.keys[i], end=' ')
+        print()
+        for item in root.childs:
+            if item is not None:
+                self._print(item)
+
+    def print(self):
+        self._print(self.root)
+
+    def _create(self):
+        x = BTree.Node(self.t)
+        x.leaf = True
+        x.n = 0
+        self.root = x
+
+    def _split_child(self, x, i, y):
+        z = BTree.Node(self.t)
+        z.leaf = y.leaf
+        z.n = self.t - 1
+        for j in range(0, self.t-1):
+            z.keys[j] = y.keys[j+self.t]
+
+        if not y.leaf:
+            for j in range(0, self.t):
+                z.childs[j] = y.childs[j+self.t]
+        y.n = self.t - 1
+
+        for j in range(x.n, i, -1):
+            x.childs[j+1] = x.childs[j]
+        x.childs[i+1] = z
+
+        for j in range(x.n-1, i-1, -1):
+            x.keys[j+1] = x.keys[j]
+        x.keys[i] = y.keys[self.t-1]
+        x.n += 1
+
+    def _insert_nonfull(self, x, k):
+        i = x.n
+        if x.leaf:
+            while i > 0 and k < x.keys[i-1]:
+                x.keys[i] = x.keys[i-1]
+                i -= 1
+            x.keys[i] = k
+            x.n += 1
+        else:
+            while i > 0 and k < x.keys[i-1]:
+                i -= 1
+            if x.childs[i].n == self.t * 2 - 1:
+                self._split_child(x, i, x.childs[i])
+
+                if k > x.keys[i]:
+                    i += 1
+
+            self._insert_nonfull(x.childs[i], k)
+
+    def insert(self, k):
+        r = self.root
+        if r.n == self.t * 2 - 1:
+            s = BTree.Node(self.t)
+            self.root = s
+            s.leaf = False
+            s.n = 0
+            s.childs[0] = r
+            self._split_child(s, 0, r)
+            self._insert_nonfull(s, k)
+        else:
+            self._insert_nonfull(r, k)
+
+
+if __name__ == "__main__":
+    from random import shuffle
+    array = [2, 3, 5, 7, 11, 13, 15, 17, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    test_times = 10000
+
+    for i in range(0, test_times):
+        # random shuffle array
+        shuffle(array)
+
+        bt = BTree(t=3)
+        for item in array:
+            print('*' * 80)
+            bt.insert(item)
+            print('**', item, '**')
+            bt.print()
+
+        print('****', i, '****')
