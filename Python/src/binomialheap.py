d82d52b0076340627a4d98ac21af53c59e396c4b
richard-ma <richard.ma.19850509@gmail.com>
2021-06-29 12:09:54 +0800

    add binomial heap merge method

--- Python/src/binomialheap.py
+++ Python/src/binomialheap.py
@@ -1,5 +1,7 @@
 from __future__ import annotations  # 必须放在第一行，为了让Node类的link方法调用Node类型的参数
+from copy import deepcopy
 from Python.src.entity import Entity
+from Python.src.helper import INT_MAX, INT_MIN
 
 
 class Node:
@@ -20,6 +22,55 @@ class Node:
 class BinomialHeap:
     def __init__(self):
         self.head = None
+
+    def minimum(self):
+        y = None
+        x = self.head
+        m = INT_MAX
+        while x is not None:
+            if x.entity.key < m:
+                m = x.entity.key
+                y = x
+            x = x.sibling
+        return y
+
+    def merge(self, other: BinomialHeap):
+        origin = self
+
+        if other.head is None:
+            return
+
+        if origin.head is None:
+            origin.head = deepcopy(other.head)  # TODO: test delete h after merged h
+            return
+
+        origin_ptr = origin.head
+        other_ptr = other.head
+
+        current_ptr = None
+        while origin_ptr is not None and other_ptr is not None:
+            if origin_ptr.entity.key <= other_ptr.entity.key:
+                current_node = origin_ptr
+                origin_ptr = origin_ptr.sibling
+            else:
+                current_node = other_ptr
+                other_ptr = other_ptr.sibling
+
+            if current_ptr is None:  # add first node
+                current_ptr = current_node
+                origin.head = current_node
+            else:
+                current_ptr.sibling = current_node
+                current_ptr = current_ptr.sibling
+
+        # add the tail of origin_ptr or other_ptr
+        if origin_ptr is not None:
+            current_ptr.sibling = origin_ptr
+        if other_ptr is not None:
+            current_ptr.sibling = other_ptr
+
+    def union(self, h2: BinomialHeap):
+        pass
 
 
 if __name__ == "__main__":
--- Python/tests/test_binomialheap.py
+++ Python/tests/test_binomialheap.py
@@ -14,6 +14,62 @@ class BinomialHeapNodeTestCase(unittest.TestCase):
         self.assertEqual(y.sibling, z_orig_child)
         self.assertEqual(z.degree, y.degree+1)
 
+    @staticmethod
+    def _create_root_list(a: list) -> Node:
+        head = None
+        current = head
+        for n in a:
+            node = Node(Entity(n))
+            if current is None:
+                current = node
+                head = current
+            else:
+                current.sibling = node
+                current = current.sibling
+        current.sibling = None
+
+        return head
+
+    @staticmethod
+    def print_root_list(head):
+        current = head
+        while current is not None:
+            print(current.entity.key, end=' -> ')
+            current = current.sibling
+        print(current)
+
+    def test_binomialheap_merge(self):
+        a1 = [7, 27, 29]
+        a2 = [5, 9, 18, 40]
+
+        print("origin & other are None")
+        origin = BinomialHeap()
+        other = BinomialHeap()
+        origin.merge(other)
+        BinomialHeapNodeTestCase.print_root_list(origin.head)
+
+        print("other is None")
+        origin = BinomialHeap()
+        other = BinomialHeap()
+        origin.head = BinomialHeapNodeTestCase._create_root_list(a1)
+        origin.merge(other)
+        BinomialHeapNodeTestCase.print_root_list(origin.head)
+
+        print("origin is None")
+        origin = BinomialHeap()
+        other = BinomialHeap()
+        other.head = BinomialHeapNodeTestCase._create_root_list(a2)
+        origin.merge(other)
+        BinomialHeapNodeTestCase.print_root_list(origin.head)
+
+        print("origin & other are not None")
+        origin = BinomialHeap()
+        other = BinomialHeap()
+        origin.head = BinomialHeapNodeTestCase._create_root_list(a1)
+        other.head = BinomialHeapNodeTestCase._create_root_list(a2)
+        origin.merge(other)
+        BinomialHeapNodeTestCase.print_root_list(origin.head)
+
 
 if __name__ == '__main__':
     unittest.main()
